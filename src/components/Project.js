0d4c4730ba3a06f0dc75126060e17ab4d4185dc5
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-04-28 12:58:50 +0530

    project

---
+++ src/components/Project.js
@@ -0,0 +1,14 @@
+// Project.js
+
+import React from 'react';
+
+function Project({ name, description }) {
+  return (
+    <div>
+      <h1 data-ns-test="project-name">{name}</h1>
+      <h6 data-ns-test="project-description">{description}</h6>
+    </div>
+  );
+}
+
+export default Project;
